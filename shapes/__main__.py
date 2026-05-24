49db21fa4557eef576245741790f227aa651aa0e
Shakeel MM <mmmshakeel@gmail.com>
2026-05-24 09:36:38 +0200

    Step 3: Add __main__.py to make the package executable

---
+++ shapes/__main__.py
@@ -0,0 +1,7 @@
+from .geometry import area_of_square, PI
+
+print("--- Running the shapes package as a script ---")
+print(f"Area of a 10x10 square is: {area_of_square(10)}")
+print(f"Value of PI is: {PI}")
+
+print(f"➡️ Inside shapes, __name__ is: {__name__}")
--- shapes/geometry.py
+++ shapes/geometry.py
@@ -5,3 +5,7 @@ def area_of_square(side):
 PI = 3.14159
 
 print(f"Inside geometry, __name__ is: {__name__}")
+
+# This only runs if we execute geometry.py directly!
+if __name__ == "__main__":
+    print(f"Inside geometry, __name__ is: {__name__}")
