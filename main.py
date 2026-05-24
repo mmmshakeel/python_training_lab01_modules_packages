7256cbcebb5d94270d59eaa2c955977e1390769a
Shakeel MM <mmmshakeel@gmail.com>
2026-05-24 09:18:11 +0200

    Step 2: Move geometry into shapes package and add __init__.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import geometry
+import shapes
 
-# Use a function from the module
-result = geometry.area_of_square(5)
+result = shapes.area_of_square(5)
 print(f"The area is: {result}") 
 
 # Use a variable from the module
-print(f"Value of PI: {geometry.PI}")
+print(f"Value of PI: {shapes.PI}")
 print(f"Inside main, __name__ is: {__name__}")
---
+++ shapes/__init__.py
@@ -0,0 +1,4 @@
+print("Initializing the shapes package...")
+
+# Expose specific tools cleanly at the package root
+from .geometry import area_of_square, PI
